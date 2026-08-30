489c9d8ae088c1d6b8b41a638297ee0892a822ca
terri-spec <terri@greatlakescounselinggroup.com>
2026-08-30 11:20:35 -0400

    Remove legacy subscriber dashboard login route

--- src/pages/masterPage.js
+++ src/pages/masterPage.js
@@ -2,5 +2,5 @@ import { authentication } from 'wix-members';
 import wixLocation from 'wix-location';
 
 authentication.onLogin(() => {
-  wixLocation.to('/subscriber-dashboard');
+  wixLocation.to('/dashboard-copy');
   });
